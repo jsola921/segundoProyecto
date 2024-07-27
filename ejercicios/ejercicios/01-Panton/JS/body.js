653297d7c7225a459f6af55eaadbeb7e9c4b45cd
jsola921 <juansola921@gmail.com>
2024-07-27 20:44:56 +0200

    ultimos cambios 27 julio
    body

--- ejercicios/ejercicios/01-Panton/JS/body.js
+++ ejercicios/ejercicios/01-Panton/JS/body.js
@@ -4,20 +4,78 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const splitText = new SplitText(".texto-incial p", {type: "words"});
 
+// Animación para img1
 gsap.fromTo(splitText.words, 
   {
     opacity: 0.5 
   }, 
   { 
     opacity: 1, 
-    duration: 1, 
+    duration: 0.5, 
     stagger: 0.5,
     scrollTrigger: {
       trigger: ".texto-incial p",
-      start: "top 90%",
-      end: "bottom 30%",
+      start: "top 70%",
+      end: "bottom 50%",
       scrub: true,
-      markers: true // Esto es solo para depuración, puedes quitarlo cuando hayas terminado
+
     }
   }
 );
+
+
+// last update img1
+gsap.fromTo(".img1", 
+  {
+    opacity: 0,
+    y: 100
+  }, 
+  {
+    opacity: 1,
+    y: 0,
+    scrollTrigger: {
+      trigger: ".img1",
+      start: "top 85%",
+      end: "top 20%",
+      scrub: true,
+
+    }
+  }
+);
+
+// last update img2
+gsap.fromTo(".img2", 
+  {
+    opacity: 0,
+    y: -100
+  }, 
+  {
+    opacity: 1,
+    y: 0,
+    scrollTrigger: {
+      trigger: ".img2",
+      start: "top 85%",
+      end: "top 20%",
+      scrub: true,
+
+    }
+  }
+);
+
+// cambio de imagenes side menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".side-menu li");
+  const img1 = document.querySelector(".img1 img");
+  const img2 = document.querySelector(".img2 img");
+
+  menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+      const newImg1 = item.getAttribute("data-img1");
+      const newImg2 = item.getAttribute("data-img2");
+
+      img1.src = newImg1;
+      img2.src = newImg2;
+    });
+  });
+});
+
